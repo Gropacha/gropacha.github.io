882c54422760d99b046efd8761228cd123b16587
Gropacha <129266488+Gropacha@users.noreply.github.com>
2023-04-16 14:23:44 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function () {
-    let count = 0;
-    const compteurHtml = document.querySelector(".game");
-    const titre = document.querySelector("h1");
-    compteurHtml.addEventListener("click", function () {
-        titre.innerHTML = `Vous avez cliqué ${++count} fois sur cette bannière`;
-    });
-});
